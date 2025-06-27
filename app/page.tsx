9d99da6a1f14f4e885774c10a9d949e1dc096d08
OrlandoMatamoros <orlando@tuimpulsalab.com>
2025-06-27 06:32:27 +0000

    Agregar Header y Footer globalmente a todas las páginas

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,15 @@
-import Header from '@/components/Header'
 import HeroSection from '@/components/HeroSection'
 import DiagnosticSection from '@/components/DiagnosticSection'
 import TeamSection from '@/components/TeamSection'
 import ContactSection from '@/components/ContactSection'
-import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <main>
-        <HeroSection />
-        <DiagnosticSection />
-        <TeamSection />
-        <ContactSection />
-      </main>
-      <Footer />
+      <HeroSection />
+      <DiagnosticSection />
+      <TeamSection />
+      <ContactSection />
     </>
   )
 }
